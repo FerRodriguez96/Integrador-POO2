b6aaa8b0ad72b42f5ae5f605bb765f02b24bcad1
Fernando <feer.rodriguez96@gmail.com>
2021-10-16 14:18:13 -0300

    #5 dev_fer - Persistencia
    Añadida capa de persistencia

---
+++ proyectoheladeria/src/main/java/com/persistencia/Persistencia.java
@@ -0,0 +1,48 @@
+package com.persistencia;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+public class Persistencia  {
+    
+    private final EntityManager em;
+        
+    public Persistencia(EntityManagerFactory emf) {
+        this.em = emf.createEntityManager();        
+    }
+    
+    public void iniciarTransaccion() {
+        em.getTransaction().begin();
+    }
+    
+    public void confirmarTransaccion() {
+        em.getTransaction().commit();
+    }
+
+    public void descartarTransaccion() {
+        em.getTransaction().rollback();
+    }
+    
+    public void insertar(Object o) {
+        this.em.persist(o);
+    }
+    
+    public void modificar(Object o){
+        this.em.merge(o);
+    }
+
+    public void eliminar(Object o){
+        this.em.remove(o);
+    }
+
+    public void refrescar(Object o) {
+        this.em.refresh(o);
+    }
+    
+    // Metodo generico
+    // Acepta cualquier tipo (T) que extienda de Object
+    // Devuelve un objeto de tipo (T)    
+    public <T extends Object> T buscar(Class<T> clase, Object id) {
+        return (T) this.em.find(clase, id);
+    }    
+}
