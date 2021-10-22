26c01f324cad62fba18947fc2daae882ac25ca3b
Fernando <feer.rodriguez96@gmail.com>
2021-10-21 22:46:30 -0300

    #11 dev_fer - cambio de paquete
    se cambio app.java a el paquete com

---
+++ proyectoheladeria/src/main/java/com/App.java
@@ -0,0 +1,13 @@
+package com;
+
+import io.javalin.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.render("index.jte"));
+    }
+}
