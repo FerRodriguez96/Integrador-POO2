f55ddf5ea8b9a7f6f292677f2d09b032e4f232d8
Fernando <feer.rodriguez96@gmail.com>
2021-10-29 09:14:14 -0300

    #2 iteracion2_fer - Clase pedidos
    correcion de errores en la clase y cambio de nombre

---
+++ proyectoheladeria/src/main/java/com/modelo/Pedidos.java
@@ -0,0 +1,122 @@
+package com.modelo;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+@Table(name="Pedidos")
+public class Pedidos implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(name="IdPedido")
+	@SequenceGenerator(name = "sec_ped",initialValue = 1,allocationSize = 1)
+    @GeneratedValue(generator = "sec_ped", strategy = GenerationType.SEQUENCE)
+	private int idPedido;
+	
+	@ManyToOne
+	@Column(name="Cliente")
+	private Cliente cliente;
+	
+	@OneToMany
+	@Column(name="Productos")
+	private String producto;
+	
+	@ManyToOne
+	@Column(name="Repartidor")
+	private Repartidor repartidor;
+	
+	@Column(name="Estado")
+	private String estado;
+
+	public Pedidos() {
+		// TODO Auto-generated constructor stub
+	}
+	
+    /**
+     * Crea un pedido al que se le pasa el cliente que hizo el pedido, los productos,
+     * y el repartidor que se va a encargar de el pedido.
+     * 
+     * @param cliente      	datos del cliente
+     * @param producto  	productos del pedido 
+     * @param repartidor 	repartidor encargado del pedido
+     */
+	public Pedidos(Cliente cliente, String producto, Repartidor repartidor) {
+		this.cliente = cliente;
+		this.producto = producto;
+		this.repartidor = repartidor;
+	}
+
+    /**
+     * Obtiene el id del pedido
+     * 
+     * @return idPedido del pedido
+     */
+	public int getIdPedido() {
+		return idPedido;
+	}
+
+    /**
+     * Establece el id del pedido
+     * 
+     * @param idPedido id del pedido
+     */
+	public void setIdPedido(int idPedido) {
+		this.idPedido = idPedido;
+	}
+
+    /**
+     * Obtiene el cliente del pedido
+     * 
+     * @return cliente del pedido
+     */
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	 /**
+     * Establece el cliente del pedido
+     * 
+     * @param cliente cliente del pedido
+     */
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+    /**
+     * Obtiene el producto del pedido
+     * 
+     * @return producto del pedido
+     */
+	public String getProducto() {
+		return producto;
+	}
+
+    /**
+     * Establece los productos del pedido
+     * 
+     * @param producto producto del pedido
+     */
+	public void setProducto(String producto) {
+		this.producto = producto;
+	}
+
+    /**
+     * Obtiene el reparidor del pedido
+     * 
+     * @return repartidor del pedido
+     */
+	public Repartidor getRepartidor() {
+		return repartidor;
+	}
+
+    /**
+     * Establece el repartidor del pedido
+     * 
+     * @param repartidor repartidor del pedido
+     */
+	public void setRepartidor(Repartidor repartidor) {
+		this.repartidor = repartidor;
+	}
+}
