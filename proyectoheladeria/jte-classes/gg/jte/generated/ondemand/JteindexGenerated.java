a949d08f007867d52bb2321066d3a67c9c1ffbd3
Fernando <feer.rodriguez96@gmail.com>
2021-10-30 04:50:02 -0300

    #4 iteracion2_fer - Correcion plantillas iteracion 1
    Se arreglaron errores de atributos en elementos de los formularios de las plantillas de la iteracion 1.
    Se agregaron los elementos de "productos" y "pedidos" a la barra de navegacion del header.

--- proyectoheladeria/jte-classes/gg/jte/generated/ondemand/JteindexGenerated.java
+++ proyectoheladeria/jte-classes/gg/jte/generated/ondemand/JteindexGenerated.java
@@ -4,9 +4,9 @@ public final class JteindexGenerated {
 	public static final int[] JTE_LINE_INFO = {0,0,0,0,0,0,25,25,26};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
 		gg.jte.generated.ondemand.tag.JteheaderGenerated.render(jteOutput, jteHtmlInterceptor);
-		jteOutput.writeContent("\n\t\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\n\t\t<div class=\"row justify-content-md-center\">\n\t\t    <div class=\"col-md-2 col-xs-6\">\n\t\t      <a href=\"/crearCliente\" class=\"btn btn-success\">Crear Cliente</a>\n\t\t    </div>\n\t\t    <div class=\"col-md-2 col-xs-6\">\n\t\t      <a href=\"/tipoEmpleado\" class=\"btn btn-success\">Crear Empleado</a>\n\t\t    </div>\n\t  \t</div>\n\t\t  \n");
+		jteOutput.writeContent("\r\n\t\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\r\n\t\t<div class=\"row justify-content-md-center\">\r\n\t\t    <div class=\"col-md-2 col-xs-6\">\r\n\t\t      <a href=\"/crearCliente\" class=\"btn btn-success\">Crear Cliente</a>\r\n\t\t    </div>\r\n\t\t    <div class=\"col-md-2 col-xs-6\">\r\n\t\t      <a href=\"/tipoEmpleado\" class=\"btn btn-success\">Crear Empleado</a>\r\n\t\t    </div>\r\n\t  \t</div>\r\n\t\t  \r\n");
 		gg.jte.generated.ondemand.tag.JtefooterGenerated.render(jteOutput, jteHtmlInterceptor);
-		jteOutput.writeContent("\n");
+		jteOutput.writeContent("\r\n");
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		render(jteOutput, jteHtmlInterceptor);
--- proyectoheladeria/jte-classes/gg/jte/generated/ondemand/JtetipoEmpleadoGenerated.java
+++ proyectoheladeria/jte-classes/gg/jte/generated/ondemand/JtetipoEmpleadoGenerated.java
@@ -4,7 +4,7 @@ public final class JtetipoEmpleadoGenerated {
 	public static final int[] JTE_LINE_INFO = {0,0,0,0,0,0,26,26};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
 		gg.jte.generated.ondemand.tag.JteheaderGenerated.render(jteOutput, jteHtmlInterceptor);
-		jteOutput.writeContent("\n\t\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\t<br>\n\n\t\t<div class=\"row justify-content-md-center\">\n\t\t    <div class=\"col-md-2 col-xs-6\">\n\t\t      <a href=\"/crearJornalero\" class=\"btn btn-success\">Jornalero</a>\n\t\t    </div>\n\t\t    <div class=\"col-md-2 col-xs-6\">\n\t\t      <a href=\"/crearRepartidor\" class=\"btn btn-success\">Repartidor</a>\n\t\t    </div>\n\t  \t</div>\n\n\n");
+		jteOutput.writeContent("\r\n\t\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\t<br>\r\n\r\n\t\t<div class=\"row justify-content-md-center\">\r\n\t\t    <div class=\"col-md-2 col-xs-6\">\r\n\t\t      <a href=\"/crearJornalero\" class=\"btn btn-success\">Jornalero</a>\r\n\t\t    </div>\r\n\t\t    <div class=\"col-md-2 col-xs-6\">\r\n\t\t      <a href=\"/crearRepartidor\" class=\"btn btn-success\">Repartidor</a>\r\n\t\t    </div>\r\n\t  \t</div>\r\n\r\n\r\n");
 		gg.jte.generated.ondemand.tag.JtefooterGenerated.render(jteOutput, jteHtmlInterceptor);
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
