d238dea177cfba5f64e81655627b3c74fc6415e2
Fernando <feer.rodriguez96@gmail.com>
2021-10-30 22:08:50 -0300

    #4 iteracion2_fer - Repositorios/jornalero
    Se añadieron los archivos necesarios en la carpeta repositorio asociados a la clase jornalero. Interfaz, repositorio.
    Se añadio el controlador asociado a la clase jornalero

--- proyectoheladeria/src/main/java/com/controladores/ControladorJornalero.java
+++ proyectoheladeria/src/main/java/com/controladores/ControladorJornalero.java
@@ -1,65 +1,94 @@
 package com.controladores;
 
+import java.sql.SQLException;
+import java.util.Collections;
+
 import com.modelo.Jornalero;
-import com.persistencia.Persistencia;
+import com.paginas.ModeloJornalero;
+import com.paginas.ModeloJornaleros;
+import com.repositorios.InterfazJornaleros;
+
+import io.javalin.http.Context;
 
 public class ControladorJornalero {
 
-    public ControladorJornalero() {
-        // TODO Auto-generated constructor stub
-    }
+	private final InterfazJornaleros interfazJornaleros;
 
-    Persistencia persistencia;
+	public ControladorJornalero(InterfazJornaleros interfazJornaleros) {
+		this.interfazJornaleros = interfazJornaleros;
+	}
 
-    public ControladorJornalero(Persistencia p) {
-        this.persistencia = p;
-    }
+	/**
+	 * @param ctx
+	 */
+	public void listar(Context ctx) throws SQLException {
+		//Se obtienen los datos de la clase
+		var modelo = new ModeloJornaleros();
+		
+		//Se pasan los datos a el metodo listar
+		modelo.jornaleros = interfazJornaleros.listar();
+		
+		//Se imprime por consola la lista de jornaleros
+		System.out.println(modelo.jornaleros);
+		
+		//Se muestra el template con la lista de jornaleros
+		ctx.render("jornaleros.jte", Collections.singletonMap("modelo", modelo));
+	}
 
-    /**
-     * @param dni
-     * @param nombre
-     * @param apellido
-     * @param telefono
-     * @param mail
-     * @param produccion
-     */
-    public void agregarJornalero(int dni, String nombre, String apellido, String telefono, String mail,
-            int produccion) {
-        this.persistencia.iniciarTransaccion();
-        Jornalero jor = new Jornalero(dni, nombre, apellido, telefono, mail);
-        this.persistencia.insertar(jor);
-        this.persistencia.confirmarTransaccion();
-    }
+	/**
+	 * @param ctx
+	 */
+	public void nuevoJornalero(Context ctx) throws SQLException {
+		// el programa musetra el formulario para ingresar los datos de un nuevo jornalero
+		ctx.render("crearJornalero.jte", Collections.singletonMap("modelo", null));
+	}
 
-    /**
-     * @param jor
-     * @param dni
-     * @param nombre
-     * @param apellido
-     * @param telefono
-     * @param mail
-     * @param produccion
-     */
-    public void editarRepartidor(Jornalero jor, int dni, String nombre, String apellido, String telefono, String mail,
-            int produccion) {
-        this.persistencia.iniciarTransaccion();
-        jor.setDni(dni);
-        jor.setNombre(nombre);
-        jor.setApellido(apellido);
-        jor.setTelefono(telefono);
-        jor.setMail(mail);
-        jor.setProduccion(produccion);
-        this.persistencia.modificar(jor);
-        this.persistencia.confirmarTransaccion();
-    }
+	/**
+	 * @param ctx
+	 */
+	public void agregarJornalero(Context ctx) throws SQLException {
 
-    /**
-     * @param jor
-     */
-    public void eliminarRepartidor(Jornalero jor) {
-        this.persistencia.iniciarTransaccion();
-        this.persistencia.eliminar(jor);
-        this.persistencia.confirmarTransaccion();
-    }
+		// Se obtienen los datos del formulario
+		var dni = ctx.formParamAsClass("txtDni", Integer.class).get();
+		var nombre = ctx.formParamAsClass("txtNombre", String.class).get();
+		var apellido = ctx.formParamAsClass("txtApellido", String.class).get();
+		var telefono = ctx.formParamAsClass("txtTelefono", String.class).get();
+		var mail = ctx.formParamAsClass("txtCorreo", String.class).get();
+
+		// Se crea un nuevo objeto jornalero
+		var jornalero = new Jornalero(dni, nombre, apellido, telefono, mail);
+
+		// Se inicia el proceso de persistencia
+		this.interfazJornaleros.crear(jornalero);
+
+		// Se redirige a la pagina que muestra la lista de jornaleros
+		ctx.redirect("/Jornaleros");
+	}
+
+	/**
+	 * @param context
+	 */
+	public void editarJornalero(Context ctx) throws SQLException {
+
+		// se traen los datos de la clase
+		var modelo = new ModeloJornalero();
+
+		// se le pasa al proceso de persistencia el dni del jornalero que se quiere
+		// editar
+		modelo.jornalero = this.interfazJornaleros.obtener(ctx.pathParamAsClass("txtDni", Integer.class).get());
+
+		// el programa muestra el template para editar jornaleros
+		ctx.render("editarJornalero.jte", Collections.singletonMap("modelo", modelo));
+	}
+
+	/**
+	 * @param context
+	 */
+	public void eliminarJornalero(Context ctx) throws SQLException {
+		// se le pasa al proceso de persistencia el dni del jornalero que se queire
+		// eliminar
+		this.interfazJornaleros.borrar(ctx.pathParamAsClass("txtDni", Integer.class).get());
+
+	}
 
 }
---
+++ proyectoheladeria/src/main/java/com/repositorios/InterfazJornaleros.java
@@ -0,0 +1,14 @@
+package com.repositorios;
+
+import java.util.List;
+
+import com.modelo.Jornalero;
+
+public interface InterfazJornaleros {
+	
+	List<Jornalero> listar();
+    void crear (Jornalero jornalero);
+    boolean borrar (int dni);
+    Jornalero obtener (int dni);
+
+}
