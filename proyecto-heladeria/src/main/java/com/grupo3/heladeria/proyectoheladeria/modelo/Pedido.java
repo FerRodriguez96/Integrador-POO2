eb265b5f220d9f6d5fd521fec8527c4d7fa53758
Fernando <feer.rodriguez96@gmail.com>
2021-11-10 09:09:14 -0300

    #2 iteracion3-fer - Version inicial clase pedido
    Se ha añadido la version inicial de la clase pedido

--- proyecto-heladeria/src/main/java/com/grupo3/heladeria/proyectoheladeria/modelo/Pedido.java
+++ proyecto-heladeria/src/main/java/com/grupo3/heladeria/proyectoheladeria/modelo/Pedido.java
@@ -1,6 +1,8 @@
 package com.grupo3.heladeria.proyectoheladeria.modelo;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +20,16 @@ public class Pedido implements Serializable {
     @GeneratedValue(generator = "sec_ped", strategy = GenerationType.SEQUENCE)
     private int idPedido;
 
+    @ManyToOne
+    @JoinColumn(name="repartidor")
+    private Repartidor repartidor;
+
+    @Column(name="fecha")
+    private LocalDate fecha;
+
+    @Column(name="horario-de-inicio")
+    private LocalDateTime horainicio;
+
     @OneToMany
     @JoinColumn(name = "Productos")
     private List<Producto>productos = new ArrayList<>();
@@ -25,73 +37,112 @@ public class Pedido implements Serializable {
     @Column(name = "preciofinal")
     private double preciofinal;
 
+    @Column(name= "estado")
+    private String estado;
+
+    @Column(name= "horario-de-entrega")
+    private LocalDateTime horaentrega;
+
+    @ManyToOne
+    @JoinColumn(name="cliente")
+    private Cliente cliente;
+
+    @OneToOne
+    private Pago pago;
+
+
     public Pedido() {
 
     }
 
-    /**
-     * Crea un pedido al que se le pasa el cliente que hizo el pedido, los
-     * productos, y el repartidor que se va a encargar de el pedido.
-     * 
-     * @param producto   productos del pedido
-     */
-    public Pedido(List<Producto> productos) {
-        this.productos = productos;
-    }
-
-
-    /**
-     * Obtiene el id del pedido
-     * 
-     * @return idPedido del pedido
-     */
     public int getIdPedido() {
         return idPedido;
     }
 
-    /**
-     * Establece el id del pedido
-     * 
-     * @param idPedido id del pedido
-     */
     public void setIdPedido(int idPedido) {
         this.idPedido = idPedido;
     }
 
-    /**
-     * Obtiene la lista de productos del pedido
-     * 
-     * @return productos del pedido
-     */
+    public Repartidor getRepartidor() {
+        return repartidor;
+    }
+
+    public void setRepartidor(Repartidor repartidor) {
+        this.repartidor = repartidor;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalDateTime getHorainicio() {
+        return horainicio;
+    }
+
+    public void setHorainicio(LocalDateTime horainicio) {
+        this.horainicio = horainicio;
+    }
+
     public List<Producto> getProductos() {
         return productos;
     }
 
-    /**
-     * Establece la lista de productos del pedido
-     * 
-     * @param productos productos del pedido
-     */
     public void setProductos(List<Producto> productos) {
         this.productos = productos;
     }
 
-    /**
-     * Obtiene el precio final del pedido
-     * 
-     * @return preciofinal del pedido
-     */
     public double getPreciofinal() {
         return preciofinal;
     }
 
-    /**
-     * Establece el preciofinal del pedido
-     * 
-     * @param preciofinal preciofinal del pedido
-     */
     public void setPreciofinal(double preciofinal) {
         this.preciofinal = preciofinal;
     }
 
+    public LocalDateTime getHoraentrega() {
+        return horaentrega;
+    }
+
+    public void setHoraentrega(LocalDateTime horaentrega) {
+        this.horaentrega = horaentrega;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Pago getPago() {
+        return pago;
+    }
+
+    public void setPago(Pago pago) {
+        this.pago = pago;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido [cliente=" + cliente + ", estado=" + estado + ", fecha=" + fecha + ", horaentrega=" + horaentrega
+                + ", horainicio=" + horainicio + ", idPedido=" + idPedido + ", preciofinal=" + preciofinal
+                + ", productos=" + productos + ", repartidor=" + repartidor + "]";
+    }
+
+    
+
+ 
 }
